f7cba7c0aab059d2516a70bf2318ebc6449e579c
Kay Sievers <kay@vrfy.org>
2021-04-30 00:32:26 +0200

    Import

---
+++ src/V2Power.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <Arduino.h>
+
+class V2Power {
+public:
+  enum Mode { Idle };
+
+  static void setSleepMode(V2Power::Mode mode) {
+    switch (mode) {
+      case Mode::Idle:
+        // Sleep mode IDLE, calling __WFI() stops the CPU, peripherals are still running.
+        PM->SLEEPCFG.bit.SLEEPMODE = 0x02;
+        while (PM->SLEEPCFG.bit.SLEEPMODE != 0x02)
+          ;
+        break;
+    }
+  }
+
+  static void sleep() {
+    __WFI();
+  }
+};
